735356110e381536c09636cba1c70da2ed13c5c4
edmasters <76230161+edmasters@users.noreply.github.com>
2021-01-30 19:21:16 -0800

    Add files via upload

--- publisher.py
+++ publisher.py
@@ -1,20 +1,30 @@
-# Publishes random stock prices
-
 import zmq
-from random import randrange
+import random
+import sys
+import time
+
+port = "5559"
 
 context = zmq.Context()
 
+#change to xpub when doing multiple
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://127.0.0.1:2000")
+socket.connect("tcp://localhost:%s" % port)
 
-stock_list = ["GOOG", "AAPL", "MSFT", "IBM", "AMD", "CLII", "EXO", "NFLX", "CME", "CKA"]
 
-#select a random stock ticker, assign a random positive price to it
+
+
 while True:
-    index = randrange(0, len(stock_list))
-    #ticker = stock_list[index]
-    ticker = "MSFT"
-    price = randrange(20, 60)
 
-    socket.send_string("%s %i" % (ticker, price))
+	#select a stock
+	stock_list = ["GOOG", "AAPL", "MSFT", "IBM", "AMD", "CLII", "EXO", "NFLX", "CME", "CKA"]
+	index = random.randrange(1,10)
+	ticker = stock_list[index]
+
+	#generate a random price
+	price = random.randrange(20, 60)
+	print (ticker + " " + str(price))
+
+	#send ticker + price to broker
+	socket.send_string("%s %i" % (ticker, price))
+	time.sleep(1)
--- subscriber.py
+++ subscriber.py
@@ -1,31 +1,18 @@
 import sys
 import zmq
 
-#  Socket to talk to server
+port = "5560"
+# Socket to talk to server
 context = zmq.Context()
-
-# Since we are the subscriber, we use the SUB type of the socket
 socket = context.socket(zmq.SUB)
 
-# Here we assume publisher runs locally unless we
-# send a command line arg like 10.0.0.1
-connect_str = "tcp://127.0.0.1:2000"
+print ("Collecting updates from server...")
+socket.connect ("tcp://localhost:%s" % port)
 
-print("Collecting stock updates...")
-socket.connect(connect_str)
-
-# Subscribe to MSFT updates
-ticker_filter = "MSFT"
-socket.setsockopt_string(zmq.SUBSCRIBE, ticker_filter)
-
-# Process 10 updates
-total_price = 0
+#filter for specific stock ticker
+topicfilter = "IBM"
+socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
 for update_nbr in range(10):
-    string = socket.recv_string()
-    ticker, price = string.split()
-    total_price += int(price)
-    print(ticker + " " + price)
-
-print("Average price for stock '%s' was %d" % (
-      ticker_filter, total_price / (update_nbr+1))
-)
+    string = socket.recv()
+    topic, messagedata = string.split()
+    print (topic, messagedata)
