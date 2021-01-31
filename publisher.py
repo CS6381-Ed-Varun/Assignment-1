e50d6116ba137fe2cca772c424a3bd1fca189cda
edmasters <76230161+edmasters@users.noreply.github.com>
2021-01-30 16:02:12 -0800

    pass stock + price locally

--- publisher.py
+++ publisher.py
@@ -1,38 +1,30 @@
-# Sample code for CS6381
-# Vanderbilt University
-# Instructor: Aniruddha Gokhale
-#
-# Code taken from ZeroMQ examples with additional
-# comments or extra statements added to make the code
-# more self-explanatory  or tweak it for our purposes
-#
 # We are executing these samples on a Mininet-emulated environment
 #
 #
-
-#
 #   Weather update server
 #   Binds PUB socket to tcp://*:5556
-#   Publishes random weather updates
+#   Publishes random stock prices
 #
 
 import zmq
 from random import randrange
 
-print("Current libzmq version is %s" % zmq.zmq_version())
-print("Current  pyzmq version is %s" % zmq.__version__)
-
 context = zmq.Context()
 
 # The difference here is that this is a publisher and its aim in life is
 # to just publish some value. The binding is as before.
+
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:5556")
+socket.bind("tcp://127.0.0.1:2000")
 
-# keep publishing 
+# keeps publishing
+stock_list = ["GOOG", "AAPL", "MSFT", "IBM", "AMD", "CLII", "EXO", "NFLX", "CME", "CKA"]
+
+#select a random stock ticker, assign a random positive price to it
 while True:
-    zipcode = randrange(1, 100000)
-    temperature = randrange(-80, 135)
-    relhumidity = randrange(10, 60)
+    index = randrange(0, len(stock_list))
+    #ticker = stock_list[index]
+    ticker = "MSFT"
+    price = randrange(20, 60)
 
-    socket.send_string("%i %i %i" % (zipcode, temperature, relhumidity))
+    socket.send_string("%s %i" % (ticker, price))
--- subscriber.py
+++ subscriber.py
@@ -1,21 +1,3 @@
-# Sample code for CS6381
-# Vanderbilt University
-# Instructor: Aniruddha Gokhale
-#
-# Code taken from ZeroMQ examples with additional
-# comments or extra statements added to make the code
-# more self-explanatory  or tweak it for our purposes
-#
-# We are executing these samples on a Mininet-emulated environment
-#
-#
-
-
-#
-#   Weather update client
-#   Connects SUB socket to tcp://localhost:5556
-#   Collects weather updates and finds avg temp in zipcode
-#
 
 import sys
 import zmq
@@ -28,30 +10,26 @@ socket = context.socket(zmq.SUB)
 
 # Here we assume publisher runs locally unless we
 # send a command line arg like 10.0.0.1
-srv_addr = sys.argv[1] if len(sys.argv) > 1 else "localhost"
-connect_str = "tcp://" + srv_addr + ":5556"
+connect_str = "tcp://127.0.0.1:2000"
 
-print("Collecting updates from weather server...")
+print("Collecting stock updates...")
 socket.connect(connect_str)
 
 # Subscribe to zipcode, default is NYC, 10001
-zip_filter = sys.argv[2] if len(sys.argv) > 2 else "10001"
-
-# Python 2 - ascii bytes to unicode str
-if isinstance(zip_filter, bytes):
-    zip_filter = zip_filter.decode('ascii')
+ticker_filter = "MSFT"
 
 # any subscriber must use the SUBSCRIBE to set a subscription, i.e., tell the
 # system what it is interested in
-socket.setsockopt_string(zmq.SUBSCRIBE, zip_filter)
+socket.setsockopt_string(zmq.SUBSCRIBE, ticker_filter)
 
-# Process 5 updates
-total_temp = 0
-for update_nbr in range(5):
+# Process 10 updates
+total_price = 0
+for update_nbr in range(10):
     string = socket.recv_string()
-    zipcode, temperature, relhumidity = string.split()
-    total_temp += int(temperature)
+    ticker, price = string.split()
+    total_price += int(price)
+    print(ticker + price)
 
-print("Average temperature for zipcode '%s' was %dF" % (
-      zip_filter, total_temp / (update_nbr+1))
+print("Average price for stock '%s' was %d" % (
+      ticker_filter, total_price / (update_nbr+1))
 )
